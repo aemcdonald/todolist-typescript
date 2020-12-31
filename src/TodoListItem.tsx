cfcbdf599a7c549853d1624fad34b27fed81c4e3
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-31 10:26:08 -0700

    Pass props as generic

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -7,7 +7,7 @@ interface TodoListItemProps {
     }
 }
 
-export const TodoListItem: React.FC = (props) => {
+export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
     return(
         <h1>TodoListItem</h1>
     )
