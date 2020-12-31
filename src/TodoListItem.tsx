8bbef70659a5c6483aa53947666672a6d70d30af
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-31 10:25:36 -0700

    Create interface

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+interface TodoListItemProps {
+    todo: {
+        text: string;
+        complete: boolean;
+    }
+}
+
 export const TodoListItem: React.FC = (props) => {
     return(
         <h1>TodoListItem</h1>
