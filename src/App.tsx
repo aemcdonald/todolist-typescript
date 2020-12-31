32da9f9857ff6ec6cc01a2fde76c38e538b13222
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-31 10:31:05 -0700

    Create type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { TodoListItem } from './TodoListItem';
 
+type Todo = {
+  text: string,
+  complete: boolean;
+}
+
 const todos = [
   { text: 'Do laundry', complete: false },
   { text: 'Grocery list', complete: true }
