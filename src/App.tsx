8935ac2c3cd161b7cb0fe56e096b3622223e14e6
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-31 10:32:48 -0700

    Remove todo type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { TodoListItem } from './TodoListItem';
 
-type Todo = {
-  text: string,
-  complete: boolean;
-}
-
 const todos: Array<Todo> = [
   { text: 'Do laundry', complete: false },
   { text: 'Grocery list', complete: true }
