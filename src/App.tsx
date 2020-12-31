dd3be6c16922b8c9650f7be1d7726c4951f1f90a
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-31 10:30:36 -0700

    Create todos array

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { TodoListItem } from './TodoListItem';
 
+const todos = [
+  { text: 'Do laundry', complete: false },
+  { text: 'Grocery list', complete: true }
+];
+
 const App: React.FC = () => {
   return <TodoListItem />;
 };
