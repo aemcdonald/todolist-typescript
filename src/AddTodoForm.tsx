ff92253067072c79929c0dcc3cebae27c3a1341d
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-01 23:04:26 -0700

    Create handleSubmit function

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 
-export const AddTodoForm: React.FC = props => {
+interface AddTodoFormProps {
+  addTodo: AddTodo;
+}
+
+export const AddTodoForm: React.FC<AddTodoFormProps> = ({ addTodo }) => {
   const [newTodo, setNewTodo] = useState('');
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewTodo(event.target.value);
   };
+
+  const handleSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    addTodo(newTodo);
+  };
+
   return (
     <form>
       <input type='text' value={newTodo} onChange={handleChange} />
-      <button type='submit'>Add Todo</button>
+      <button type='submit' onClick={handleSubmit}>Add Todo</button>
     </form>
   );
 };
