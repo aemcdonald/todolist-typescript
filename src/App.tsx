4883d430b596c9d11cd5e9f73bb951607954af18
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-01 22:24:54 -0700

    Create useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { TodoListItem } from './TodoListItem';
 
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   { text: 'Do laundry', complete: false },
   { text: 'Grocery list', complete: true }
 ];
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos)
   return (<>
     <TodoListItem todo={todos[0]}/>
   </>)
