66baba28c4cc82d444d6009a1eae4c0976f4031b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-01 22:50:41 -0700

    Create handleChange function

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 
 export const AddTodoForm: React.FC = props => {
   const [newTodo, setNewTodo] = useState('');
+
+  const handleChange = event => {
+    setNewTodo(event.target.value);
+  };
   return (
     <form>
-      <input type='text' value={newTodo} />
+      <input type='text' value={newTodo} onChange={handleChange} />
       <button type='submit'>Add Todo</button>
     </form>
   );
