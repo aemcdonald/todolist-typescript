0114417e35ddc65f95c0e03d29c95d73e3dcec88
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2021-01-01 22:48:38 -0700

    Add useState hook

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const AddTodoForm: React.FC = props => {
+  const [newTodo, setNewTodo] = useState('');
   return (
     <form>
       <input type='text' />
